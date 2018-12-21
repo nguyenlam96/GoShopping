fcfe0d3f37f395b5a35b2ba8bcec4cefd866f2ce
nguyenlam455 <livefortoday01455@gmail.com>
2018-12-21 17:58:14 +0700

    Load and Delete GroceryItem

--- GoShopping/Controller/SearchItemViewController.swift
+++ GoShopping/Controller/SearchItemViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import KRProgressHUD
 
 class SearchItemViewController: UIViewController {
     
     // MARK: - Properties
     @IBOutlet weak var tableView: UITableView!
-    var items: [ShoppingItem] = []
+    var groceryItems: [GroceryItem] = []
     var theShoppingList: ShoppingList?
     // MARK: - IBOutlet
 
@@ -21,6 +22,7 @@ class SearchItemViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        loadItems()
     }
     
     // MARK: - Setup
@@ -38,7 +40,52 @@ class SearchItemViewController: UIViewController {
     
     
     // MARK: - Helper Functions
+    func loadItems() {
+        
+        firebaseRootRef.child(kGROCERYITEM).child("1234").observe(.value) { [unowned self](snapshot) in
+            
+            self.groceryItems.removeAll()
+            
+            if snapshot.exists() {
+                
+                let groceryList = (snapshot.value as! NSDictionary).allValues as NSArray
+                
+                for each in groceryList {
+                    let itemDictionary = each as! [String:Any]
+                    if self.isDataComeBackValid(item: itemDictionary) {
+                        let currentItem = GroceryItem(dictionary: itemDictionary)
+                        self.groceryItems.append(currentItem)
+                    } else {
+                        continue
+                    }
+                }
+                self.tableView.reloadData()
+            } else {
+                KRProgressHUD.showInfo(withMessage: "No snapshot return")
+                return
+            }
+            
+        }
+    }
     
+    func isDataComeBackValid(item: [String:Any]) -> Bool {
+        guard
+            let _ = item[kNAME] as? String,
+            let _ = item[kINFO] as? String,
+            let _ = item[kPRICE] as? Float,
+            let _ = item[kOWNERID] as? String,
+            let _ = item[kIMAGE] as? String,
+            let _ = item[kGROCERYITEMID] as? String
+        else {
+                return false
+        }
+        
+        return true
+    }
+    
+    func updateUI() {
+    
+    }
     
 }
 
@@ -46,14 +93,14 @@ extension SearchItemViewController: UITableViewDelegate, UITableViewDataSource {
     
     // MARK: - TableView Datasource Methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return groceryItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "searchItem", for: indexPath)
-        let item = items[indexPath.row]
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "searchItem", for: indexPath) as! SearchItemTableViewCell
+        let theItem = groceryItems[indexPath.row]
+        cell.bindData(theItem: theItem)
         return cell
         
     }
@@ -61,6 +108,21 @@ extension SearchItemViewController: UITableViewDelegate, UITableViewDataSource {
     // MARK: - TableView Delegate Methods
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 93
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // delete Item
+            let deleteItem = groceryItems[indexPath.row]
+            deleteItem.deleteItemBackground(groceryItem: deleteItem)
+            groceryItems.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
     }
     
 }
